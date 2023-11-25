99c1836025ee836c141396f19ad8848088346949
BuiTriThuc <20088361.thuc@student.iuh.edu.vn>
2023-11-25 13:06:27 +0700

    init project

---
+++ src/main/java/vn/edu/iuh/fit/wwwlabweek10sercurity/WwwLabWeek10SercurityApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.wwwlabweek10sercurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwLabWeek10SercurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwLabWeek10SercurityApplication.class, args);
+    }
+
+}
